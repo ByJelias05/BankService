7ff9115e65510dffa617b83a6e5645e048ee2d96
ByJelias05 <garciassierra@hotmail.com>
2026-06-09 10:55:58 -0400

    Server.js configured to launch the app

--- backend-banking/src/server.js
+++ backend-banking/src/server.js
@@ -1,10 +1,6 @@
-const express = require('express');
+const app = require("./app");
 
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
