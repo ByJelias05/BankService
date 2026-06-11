25664e4b49f93d939ad48b61beaeef79748a52c5
ByJelias05 <garciassierra@hotmail.com>
2026-06-11 10:29:17 -0400

    The log class was created, along with its two functions CreateLog and AddLog.

--- backend-banking/src/shared/logger/Logs.js
+++ backend-banking/src/shared/logger/Logs.js
@@ -1,7 +1,25 @@
-class NotFound extends Error{
-    constructor(){
-        super("error, usuario no encotrado.")
+const fs = require("fs")
+const GenerateFileName = require("../utils/GenerateFileName");
+const { DATE } = require("sequelize");
+
+
+class LogService{
+
+    static CreateLog(){
+        const fileName = GenerateFileName();
+        fs.writeFileSync(fileName, "Initialized banking process \n")
+        console.log(__dirname)
+    }
+
+    static AddLog(type, level, message){
+
+        const fecha = new Date
+
+        const ExtractName = GenerateFileName();
+        const NewLine = `[${fecha.toISOString()}] - [${type}] - [${level}]  [${message}] \n`
+        fs.appendFileSync(ExtractName, NewLine)
     }
 }
 
-throw new NotFound
+LogService.CreateLog();
+LogService.AddLog("SUCCESS", "MENOR", "USUARIO AUTENTICADO CON EXITOS");
